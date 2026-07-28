b96239481409f6249aa43b9af6176ab93756aa97
jasseramir <jasser.amir.work@gmail.com>
2026-07-29 00:11:43 +0300

    add nav logic

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,23 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
+if (navToggle) {
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+    });
+}
+
+if (navClose) {
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(navLink => navLink.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+}));
