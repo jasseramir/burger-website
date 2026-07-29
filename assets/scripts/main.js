fc00ddf56f5a158a3512097f5a3bf68aabf64e36
jasseramir <jasser.amir.work@gmail.com>
2026-07-29 04:45:32 +0300

    add why us section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,31 +1,34 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu');
-const navToggle = document.getElementById('nav-toggle');
-const navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
 if (navToggle) {
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu');
-    });
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 if (navClose) {
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu');
-    });
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLinks = document.querySelectorAll('.nav-link');
+const navLinks = document.querySelectorAll(".nav-link");
 
-navLinks.forEach(navLink => navLink.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu');
-}));
+navLinks.forEach((navLink) =>
+  navLink.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  }),
+);
 
 /*=============== ADD SHADOW HEADER ===============*/
-const header = document.getElementById('header');
+const header = document.getElementById("header");
 
-window.addEventListener('scroll', () => {
-    window.scrollY >= 70 ? header.classList.add('shadow-header')
-                         : header.classList.remove('shadow-header');
+window.addEventListener("scroll", () => {
+  window.scrollY >= 70
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
 });
